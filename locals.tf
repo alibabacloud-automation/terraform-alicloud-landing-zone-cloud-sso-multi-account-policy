6111737c221ab5a2431d8e234f9acce7a281d2ad
xiaozhu36 <guimin.hgm@alibaba-inc.com>
2021-12-06 17:41:36 +0800

    Adds new parameter create_resource_manager_folder to support creating new folder

--- locals.tf
+++ locals.tf
@@ -1,12 +1,12 @@
 locals {
   # If self-define filter condition is not set, using account name as default filter value.
-  group_name_regex  = var.user_group_name_regex == "" ? format(".*%s", var.display_name) : var.user_group_name_regex
+  group_name_regex  = var.cloud_sso_group_name == "" ? format(".*%s", var.display_name) : var.cloud_sso_group_name
   this_directory_id = var.create_directory ? concat(alicloud_cloud_sso_directory.this.*.id, [""])[0] : var.directory_id
   this_account_id   = concat(alicloud_resource_manager_account.this.*.id, [""])[0]
   matched_groups    = data.alicloud_cloud_sso_groups.this.groups
 
   # Get a folder id from datasource
-  this_folder_id = var.folder_id != "" ? var.folder_id : var.folder_name != "" ? concat(data.alicloud_resource_manager_folders.this.folders.*.folder_id, [""])[0] : ""
+  this_folder_id = var.folder_name == "" ? "" : length(data.alicloud_resource_manager_folders.this.folders) > 0 ? concat(data.alicloud_resource_manager_folders.this.folders.*.folder_id, [""])[0] : var.create_resource_manager_folder ? concat(alicloud_resource_manager_folder.this.*.id, [""])[0] : ""
 
   # Split all filtered group names and then using them to locate eligible access configurations
   matched_group_names_split     = distinct(flatten([for group in local.matched_groups : split(format("%s-", var.display_name), group.group_name)]))
