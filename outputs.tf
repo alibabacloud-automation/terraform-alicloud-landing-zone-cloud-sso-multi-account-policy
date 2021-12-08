3e15c21376923a9ccde107b06356c82f37fc251f
xiaozhu36 <guimin.hgm@alibaba-inc.com>
2021-12-08 14:45:11 +0800

    Updates output variables

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
-output "this_directory" {
+output "directory_id" {
   description = "The id of cloud sso directory of this module used."
   value       = local.this_directory_id
 }
-output "this_folder_id" {
+output "folder_id" {
   description = "The id of resource manager folder."
   value       = local.this_folder_id
 }
-output "this_account_id" {
+output "account_id" {
   description = "The id of resource manager account."
   value       = local.this_account_id
 }
-output "this_group_ids" {
+output "group_ids" {
   description = "List of ids of cloud sso user group."
   value       = local.matched_groups.*.group_id
 }
-output "this_group_names" {
+output "group_names" {
   description = "List of names of cloud sso user group."
   value       = local.matched_groups.*.group_name
 }
 
-output "this_access_configuration_ids" {
+output "access_configuration_ids" {
   description = "List of ids of cloud sso access configuration."
   value       = local.matched_access_configurations.*.access_configuration_id
 }
-output "this_access_configuration_names" {
+output "access_configuration_names" {
   description = "List of names of cloud sso access configuration."
   value       = local.matched_access_configurations.*.access_configuration_name
 }
