b76cc074a554c240dcf864e52bcab8f3e8c7d2bc
xiaozhu36 <guimin.hgm@alibaba-inc.com>
2021-12-08 16:56:13 +0800

    Improve cloud sso assignment judgment

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals {
   # If self-define filter condition is not set, using account name as default filter value.
   group_name_regex  = var.cloud_sso_group_name_regex == "" ? format(".*%s", var.display_name) : var.cloud_sso_group_name_regex
   this_directory_id = var.create_directory ? concat(alicloud_cloud_sso_directory.this.*.id, [""])[0] : var.directory_id != "" ? var.directory_id : concat(data.alicloud_cloud_sso_directories.default.ids, [""])[0]
-  this_account_id   = var.create_resource_manager_account ? concat(alicloud_resource_manager_account.this.*.id, [""])[0] : var.account_id
+  this_account_id   = var.create_resource_manager_account ? concat(alicloud_resource_manager_account.this.*.id, [""])[0] : var.account_id != "" ? var.account_id : ""
   matched_groups    = data.alicloud_cloud_sso_groups.this.groups
 
   # Get a folder id from datasource
